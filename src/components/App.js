f4dda6e77e0c9a8ec440f761cdcc1037809fbdd4
Joni Sadler <joni.anne.sadler@gmail.com>
2021-01-10 12:57:29 -0500

    Exercise 4 complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,13 @@ import React, { useContext } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import {GameContext} from "./GameContext";
 
+
 import GlobalStyles from "./GlobalStyles";
 import Home from "./Home";
 import Game from "./Game";
 import useInterval from "../hooks/use-interval.hook";
+
+const { items } = require("./data");
 
 function App(props) {
   const {numCookies, setNumCookies, cookiesPerSecond} = useContext(GameContext);
@@ -13,6 +16,19 @@ function App(props) {
     useInterval(() => {
     setNumCookies(numCookies + cookiesPerSecond);
   }, 1000);
+
+  window.addEventListener('unload', (event) => {
+    let exitTime = JSON.stringify(Math.floor(Date.now() / 1000));
+    localStorage.setItem('exitedWindow', exitTime)
+  });
+
+  window.addEventListener('load', (event) => {
+    let loadTime = JSON.stringify(Math.floor(Date.now() / 1000));
+    let timeClosed = localStorage.getItem('exitedWindow');
+    let timeAway = loadTime - timeClosed;
+    let accumulatedCookies = timeAway * cookiesPerSecond;
+    setNumCookies(numCookies + accumulatedCookies);
+  });
 
   return (
     <>
